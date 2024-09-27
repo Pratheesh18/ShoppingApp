08d89883355c835eb9a801288065fc1bd6f7de29
Pratheesh18 <pratheesh1218@gmail.com>
2024-09-27 20:34:49 +0530

    Product Card is done

---
+++ screens/ProductCard.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { View,Text,TouchableOpacity,Image } from 'react-native';
+import { styled } from 'nativewind';
+
+
+const Card = styled(TouchableOpacity);
+const StyledView = styled(View);
+const StyledText = styled(Text);
+
+const ProductCard = ({product , onPress}) => {
+    return(
+        <Card className='bg-white rounded-lg shadow p-4 mb-4 mt-10' onPress={onPress}>
+            <Image source={{uri:product.mainImage}} style={{width:'100%',height:160,borderRadius:8}} resizeMode="cover" />
+            <StyledView className='mt-2'>
+                <StyledText className='text-lg font-bold'> {product.name} </StyledText>
+                <StyledText className='text-sm text-gray-500'> {product.price.amount} </StyledText>
+            </StyledView>
+
+        </Card>
+    )
+};
+
+export default ProductCard;
